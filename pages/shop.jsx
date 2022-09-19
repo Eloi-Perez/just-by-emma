9514eec07dbeba7858ac38dbda0686c8bc52e90e
Eloi-Perez <eloi104@hotmail.com>
2022-09-19 10:55:53 +0100

    feat: add shop page + sass

---
+++ pages/shop.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+
+import s from '../styles/shop.module.scss'
+
+export default function Shop({ arrayProducts }) {
+  return (
+    <div className={s.container}>
+      <main className={s.main}>
+        <h1>Hi</h1>
+        {arrayProducts.map((product) => (
+          <div key={product._id}>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <h3>Price: £{product.price}</h3>
+            <hr />
+          </div>
+        ))}
+      </main>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+
+  const res = await fetch(`${process.env.BACKEND}/products`)
+  const arrayProducts = await res.json()
+
+  return {
+    props: {
+      arrayProducts,
+    },
+  }
+}
