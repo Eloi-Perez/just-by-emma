f13a560aaca105e63c35b8900403802ef8f77ad0
Eloi-Perez <eloi104@hotmail.com>
2022-09-30 12:53:22 +0100

    feat: link to admin page

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,11 +1,12 @@
-import s from './footer.module.css'
+import Link from 'next/link'
+
+import s from './footer.module.scss'
 
 export default function Footer() {
     return (
-        <footer>
-            <div>
-                This is a Footer
-            </div>
+        <footer className={s.root}>
+            <p><s>This is a Footer</s></p>
+            <p><Link href="/admin"><a>Admin Page</a></Link></p>
         </footer>
     )
 }
