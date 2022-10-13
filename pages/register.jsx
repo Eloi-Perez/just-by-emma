4db03434213d457476be58b99a2978c75f279189
Begli Kakabalov <begli.kaka@mail.ru>
2022-10-13 10:25:16 +0200

    feat.rename create pages to register page

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CreateStyle from '../styles/create.module.scss'
+import CreateStyle from '../styles/register.module.scss'
 import Link from 'next/link'
 
 const CreateAccount = () => {
