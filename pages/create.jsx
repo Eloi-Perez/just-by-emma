800b5cdf7b4f6c0cfe157d32aaae948770a09d87
Begli Kakabalov <begli.kaka@mail.ru>
2022-10-13 10:08:03 +0200

    feat.create and sign-in pages

---
+++ pages/create.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import CreateStyle from '../styles/create.module.scss'
+import Link from 'next/link'
+
+const CreateAccount = () => {
+  return (
+    <div className={CreateStyle.container}>
+      <div className={CreateStyle.wrapper}>
+        <img
+          className={CreateStyle.image}
+          src='https://unsplash.com/photos/0XnuFXiMLTk'
+          alt=''
+        />
+        <p className={CreateStyle.description}>
+          A natural skincare range. Just simple 100% natural ingredients,
+          lovingly handmade. Keeping your skincare routine simple and natural
+        </p>
+        <p className={CreateStyle.title}>Create Account</p>
+        <form className={CreateStyle.form}>
+          <input className={CreateStyle.input} type='text' />
+          <input
+            className={CreateStyle.input}
+            type='text'
+            placeholder='First Name'
+          />
+          <input
+            className={CreateStyle.input}
+            type='text'
+            placeholder='Last Name'
+          />
+          <input
+            className={CreateStyle.input}
+            type='text'
+            placeholder='Email'
+          />
+          <input
+            className={CreateStyle.input}
+            type='text'
+            placeholder='Password'
+          />
+        </form>
+
+        <button className={CreateStyle.button}>Create Account</button>
+        <p>Guest Option</p>
+      </div>
+    </div>
+  )
+}
+
+export default CreateAccount
---
+++ pages/sign-in.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import signinStyle from '../styles/sign-in.module.scss'
+import Link from 'next/link'
+
+const CreateAccount = () => {
+  return (
+    <div className={signinStyle.container}>
+      <div className={signinStyle.wrapper}>
+        <img className={signinStyle.image} src='' alt='' />
+        <p className={signinStyle.description}>
+          A natural skincare range. Just simple 100% natural ingredients,
+          lovingly handmade. Keeping your skincare routine simple and natural
+        </p>
+        <p className={signinStyle.title}>Sign in</p>
+        <p>Welcome again please sign in</p>
+        <form className={signinStyle.form}>
+          <input className={signinStyle.input} type='text' />
+          <input
+            className={signinStyle.input}
+            type='text'
+            placeholder='First Name'
+          />
+          <input
+            className={signinStyle.input}
+            type='text'
+            placeholder='Last Name'
+          />
+          <input
+            className={signinStyle.input}
+            type='text'
+            placeholder='Email'
+          />
+          <input
+            className={signinStyle.input}
+            type='text'
+            placeholder='Password'
+          />
+        </form>
+        <div className={signinStyle.buttonContainer}>
+          <button className={signinStyle.login}>Login</button>
+          <button className={signinStyle.create}>Create Account</button>
+        </div>
+
+        <p>Guest Option</p>
+      </div>
+    </div>
+  )
+}
+
+export default CreateAccount
