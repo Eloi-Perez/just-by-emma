1c6a45f1124e0c67951005a951868ab35a602601
Eloi-Perez <eloi104@hotmail.com>
2022-10-31 00:11:02 +0000

    chore: indenting

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 import s from './footer.module.scss'
 
 export default function Footer() {
-    return (
-        <footer className={s.root}>
-            <p><s>This is a Footer</s></p>
-            <p><Link href="/admin"><a>Admin Page</a></Link></p>
-        </footer>
-    )
+  return (
+    <footer className={s.root}>
+      <p><s>This is a Footer</s></p>
+      <p><Link href="/admin">Admin Page</Link></p>
+    </footer>
+  )
 }
