72aa3be0e62534b4af5bc3affe3953ba42a7c850
Eloi-Perez <eloi104@hotmail.com>
2022-11-02 11:39:48 +0000

    feat: add optimized svg logo

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 
+import Logo from '../UI/logo'
 import Search from './search'
 import s from './navbar.module.scss'
 
 export default function Navbar() {
   return (
     <nav className={s.root}>
+      <div className={s.logo}><Logo /></div>
       <Link href="/">Home</Link>
       <Link href="/shop">Shop</Link>
       <Link href="/ingredients">Ingredients</Link>
