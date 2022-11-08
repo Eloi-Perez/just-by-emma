bb980ae463e27225c94d4f82128d824b2f606b39
Eloi-Perez <eloi104@hotmail.com>
2022-11-08 16:05:53 +0000

    feat: add FB & IG temp links

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
 
+import Facebook from '../UI/svg/facebook'
+import Instagram from '../UI/svg/instagram'
 import s from './footer.module.scss'
 
 export default function Footer() {
   return (
     <footer className={s.root}>
-      <p><s>This is a Footer</s></p>
+      <a href="https://facebook.com" target="_blank"><Facebook /></a>
+      <a href="https://instagram.com" target="_blank"><Instagram /></a>
       <p><Link href="/admin">Admin Page</Link></p>
     </footer>
   )
