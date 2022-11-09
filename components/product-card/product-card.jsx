2ea3eb86110f05d1f1f62e396f63bf501e44a3cb
Eloi-Perez <eloi104@hotmail.com>
2022-11-09 00:46:11 +0000

    feat: add product page & product card

---
+++ components/product-card/product-card.jsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import s from './product-card.module.css'
+
+export default function ProductCard({ product }) {
+  return (
+    <div className={s.root}>
+      <Link href={`/shop/${product._id}`}>
+        <h3>{product.name}</h3>
+        {product.images[0] && product.images.map((img) => (
+          <Image src={`/backend/img/${img.filename}`}
+            key={img.filename}
+            alt=""
+            width={100}
+            height={100}
+            // fill
+            style={{ objectFit: 'cover' }}
+            sizes="20vw"
+          // priority
+          />
+        ))}
+        <p>{product.description}</p>
+        <h3>Price: £{product.price}</h3>
+      </Link>
+      <hr />
+    </div>
+  )
+}
--- pages/shop.jsx
+++ pages/shop.jsx
@@ -1,40 +1,20 @@
-import Image from 'next/image'
-
+import ProductCard from '../components/product-card/product-card'
 import s from '../styles/shop.module.scss'
 
 export default function Shop({ arrayProducts }) {
   return (
-    <div className={s.main_container}>
-        <h1>Hi</h1>
+    <div className={s.root}>
+        <h1>Shop</h1>
         {arrayProducts.map((product) => ( //TODO export to component product-card
-          <div key={product._id}>
-            <h3>{product.name}</h3>
-            {product.images[0] && product.images.map((img) => (
-              <Image src={`/backend/img/${img.filename}`}
-                key={img.filename}
-                alt=""
-                width={100}
-                height={100}
-                // fill
-                style={{ objectFit: 'cover' }}
-                sizes="20vw"
-                // priority
-              />
-            ))}
-            <p>{product.description}</p>
-            <h3>Price: £{product.price}</h3>
-            <hr />
-          </div>
+          <ProductCard key={product._id} product={product}/>
         ))}
     </div>
   )
 }
 
 export async function getStaticProps() {
-
   const res = await fetch(`${process.env.BACKEND}/products`)
   const arrayProducts = await res.json()
-
   return {
     props: {
       arrayProducts,
