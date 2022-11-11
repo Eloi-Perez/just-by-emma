9a839953f2c5e92eb793f6ce71b19ba7f1cd9bcf
Eloi-Perez <eloi104@hotmail.com>
2022-11-11 23:34:07 +0000

    feat: add to cart form & context

--- contexts/cart-context.js
+++ contexts/cart-context.js
@@ -1,12 +1,65 @@
 import { useReducer, createContext } from 'react'
 
 const cartReducer = (state, action) => {
+  // redeclare to avoid page not refreshing with new result if return {state}
+  let newState = { ...state }
+  const match = newState.cart.findIndex(e => e.id === action.payload.id)
+  const matchSize = (match !== -1) ? newState.cart[match].size[action.payload.sizeTo] : undefined
   switch (action.type) {
     case 'SET_CART':
       return {
         ...state,
         cart: action.payload,
       }
+    case 'EMPTY_CART':
+      return {
+        ...state,
+        cart: []
+      }
+    // case 'ADD_TO_CART':
+    case 'MODIFY_CART':
+      // { id, size: { [size]: quantity } }
+      if (match === -1) {
+        newState.cart.push(action.payload)
+      } else {
+        newState.cart[match] = {
+          ...newState.cart[match],
+          size: { ...newState.cart[match].size, ...action.payload.size }
+        }
+      }
+      return newState
+    case 'REMOVE_FROM_CART':
+      // { id, sizeTo: {size} } }
+      if (match >= 0 && matchSize) {
+        delete newState.cart[match].size[action.payload.sizeTo]
+      } else {
+        console.error('Nothing to delete')
+      }
+      return newState
+    case 'ADD_QUANTITY':
+      // { id, sizeTo: {size} } }
+      if (match === -1) { // ID not in cart
+        newState.cart.push({
+          id: action.payload.id,
+          size: { [action.payload.sizeTo]: 1 },
+        })
+      } else if (match >= 0 && !matchSize) { // size not in cart
+        newState.cart[match].size[action.payload.sizeTo] = 1
+      } else {
+        console.log('test2')
+        newState.cart[match].size[action.payload.sizeTo] += 1
+      }
+      return newState
+    case 'SUB_QUANTITY':
+      // { id, sizeTo: {size} } }
+      if (match >= 0 && matchSize === 1) {
+        delete newState.cart[match].size[action.payload.sizeTo]
+      } else if (match >= 0 && matchSize > 1) {
+        newState.cart[match].size[action.payload.sizeTo] -= 1
+      } else {
+        console.error('Nothing to delete')
+      }
+      return newState
     default:
       return state
   }
@@ -16,15 +69,19 @@ export const CartContext = createContext()
 
 export const CartProvider = props => {
   const initialState = {
-    cart: [
-      {id: 'idNumber', name: 'newest item', quantity: 2}
-    ]
+    cart:
+      [// example data
+        { id: 'idNumber', size: { s15ml: 2, s50ml: 0 } }
+        // { id: 'idNumber', size: '15ml', quantity: 2 }
+        // OR [{ id: 'the-product-id', variants: { small: 2, medium: 1 } }]
+      ]
   }
 
   const [state, dispatch] = useReducer(cartReducer, initialState)
 
-  const setCart = newCart => {
-    dispatch({ type: 'SET_CART', payload: newCart })
+  const setCart = (type, newCart) => {
+    dispatch({ type: type, payload: newCart })
+    // dispatch({ type: 'SET_CART', payload: newCart })
   }
 
   return (
--- pages/shop/[id].jsx
+++ pages/shop/[id].jsx
@@ -1,13 +1,61 @@
-import ProductCard from "../../components/product-card/product-card"
+import { useState, useContext } from 'react'
+import Image from 'next/image'
+
+import { CartContext } from '../../contexts/cart-context'
 
 // import s from '../../styles/product.module.scss'
 import s2 from '../../styles/shop.module.scss'
 
 export default function Product({ product }) {
+  const { cart, setCart } = useContext(CartContext)
+  const [size, setSize] = useState('0')
+
+  function handleAdd() {
+    if (size !== '0') {
+      setCart('ADD_QUANTITY', {
+        id: product._id,
+        sizeTo: size
+      })
+    } else {
+      console.error('error, no size selected')
+    }
+  }
 
   return (
     <div className={s2.root}>
-      <ProductCard product={product} />
+      {/* delete next 2 lines */}
+      <h3>Cart Content</h3>
+      <p>{JSON.stringify(cart)}</p>
+      <h3>{product.name}</h3>
+      {product.images[0] && product.images.map((img) => (
+        <Image src={`/backend/img/${img.filename}`}
+          key={img.filename}
+          alt=""
+          width={100}
+          height={100}
+          // fill
+          style={{ objectFit: 'cover' }}
+          sizes="20vw"
+          priority
+        />
+      ))}
+      <p>{product.description}</p>
+      <h3>From: £{product.price}</h3>
+      <div>
+        <div>Size</div>
+        <select value={size} onChange={e => setSize(e.target.value)}>
+          <option value="0">Select</option>
+          <option value="s50ml">50ml £18.00</option>
+          <option value="s15ml">15ml £ 5.00</option>
+        </select>
+      </div>
+      <br />
+      <button onClick={handleAdd}>Add to Cart</button>
+      <br /><br />
+      <div>
+        <h3>Key Ingredients</h3>
+        <p>list...</p>
+      </div>
     </div>
   )
 }
@@ -26,7 +74,6 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
   const res = await fetch(`${process.env.BACKEND}/products/${context.params.id}`)
-  console.log(context)
   const product = await res.json()
   return {
     props: { product },
