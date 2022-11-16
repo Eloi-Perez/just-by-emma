8a6ebb1e9c9d50bbfb7ff4ec57a569676ebb5814
adpenu <ovidiu_penu@yahoo.com>
2022-11-16 17:27:48 +0800

    feat: navbar grid

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -6,14 +6,21 @@ import s from './navbar.module.scss'
 export default function Navbar() {
   return (
     <nav className={s.root}>
-      <Link href="/">Home</Link>
-      <Link href="/shop">Shop</Link>
-      <Link href="/ingredients">Ingredients</Link>
-      <Link href="/register">Sign up</Link>
-      <Link href="/about">About</Link>
-      <Link href="/news">News</Link>
-      <Link href="/contact">Contact</Link>
-      <Search />
+    <div className={s.logo}>
+    logo
+    </div>
+    <div className={s.top_elements}>
+    <Search />
+    </div>
+    <div className={s.menu}>
+    <Link href="/">Home</Link>
+    <Link href="/shop">Shop</Link>
+    <Link href="/ingredients">Ingredients</Link>
+    <Link href="/register">Sign up</Link>
+    <Link href="/about">About</Link>
+    <Link href="/news">News</Link>
+    <Link href="/contact">Contact</Link>
+    </div>
     </nav>
   )
 }
