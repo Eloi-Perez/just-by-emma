b2df70c44511aeeea0b19c43e714ae398caf0548
adpenu <ovidiu_penu@yahoo.com>
2022-11-16 18:37:09 +0800

    feat: navbar elements

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -7,21 +7,25 @@ import s from './navbar.module.scss'
 export default function Navbar() {
   return (
     <nav className={s.root}>
-    <div className={s.logo}>
-    logo
-    </div>
-    <div className={s.top_elements}>
-    <Search />
-    </div>
-    <div className={s.menu}>
-    <Link href="/">Home</Link>
-    <Link href="/shop">Shop</Link>
-    <Link href="/ingredients">Ingredients</Link>
-    <Link href="/register">Sign up</Link>
-    <Link href="/about">About</Link>
-    <Link href="/news">News</Link>
-    <Link href="/contact">Contact</Link>
-    </div>
+      <div className={s.logo}>
+        <Logo />
+      </div>
+      <div className={s.top_elements}>
+        <div className={s.nav_text}>
+          Just 100% natural and simple skincare
+      </div>
+        <Search />
+        <div>temporary</div>
+      </div>
+      <div className={s.menu}>
+        <Link href="/">Home</Link>
+        <Link href="/shop">Shop</Link>
+        <Link href="/ingredients">Ingredients</Link>
+        <Link href="/register">Sign up</Link>
+        <Link href="/about">About</Link>
+        <Link href="/news">News</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
     </nav>
   )
 }
