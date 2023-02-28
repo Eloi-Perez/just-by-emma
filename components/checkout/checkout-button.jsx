b404f61308e3ebb506efb4738ed0a6d116b76424
Eloi-Perez <eloi104@hotmail.com>
2023-02-28 20:59:13 +0000

    feat: stripe integration

---
+++ components/checkout/checkout-button.jsx
@@ -0,0 +1,101 @@
+import { useContext, useEffect } from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+
+import { CartContext } from '../../contexts/cart-context'
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+)
+export default function CheckoutButton() {
+  const { cart } = useContext(CartContext)
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search)
+    if (query.get('success')) {
+      console.log('Order placed! You will receive an email confirmation.')
+    }
+
+    if (query.get('canceled')) {
+      console.log('Order canceled -- continue to shop around and checkout when you’re ready.')
+    }
+  }, [])
+
+  const checkout = async (e) => {
+    e.preventDefault()
+    let line_items = []
+    cart.map(p => {
+      p.quantities.map(item => {
+        line_items.push({
+          name: p.product.name,
+          description: item.size ,
+          amount: p.product.sizes.find(el => el.name === item.size).price * 100,
+          currency: 'GBP',
+          quantity: item.quantity,
+          // images: [`/backend/img/${p.product.images[0].filename}`], // TODO check URL for production
+          images: ['https://eloiperez.com/photography/img/food/02.webp'], // TODO remove
+        })
+      })
+    })
+    console.log(line_items)
+
+    const data = { line_items }
+    try {
+      const call = await fetch('/api/checkout_sessions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+      })
+      const response = await call.json()
+      if (call.ok) {
+        console.log(response)
+        window.location.href = response.url // or send to iframe
+      } else {
+        console.log(response)
+      }
+    } catch (error) {
+      console.error('An unexpected error happened:', error)
+    }
+  }
+
+  return (
+    <form onSubmit={checkout}>
+      {/* <form action="/api/checkout_sessions" method="POST"> */}
+      <section>
+        <button type="submit" role="link">
+          Checkout
+        </button>
+      </section>
+      <style jsx>
+        {`
+          section {
+            background: #ffffff;
+            display: flex;
+            flex-direction: column;
+            width: 400px;
+            height: 112px;
+            border-radius: 6px;
+            justify-content: space-between;
+          }
+          button {
+            height: 36px;
+            background: #556cd6;
+            border-radius: 4px;
+            color: white;
+            border: 0;
+            font-weight: 600;
+            cursor: pointer;
+            transition: all 0.2s ease;
+            box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
+          }
+          button:hover {
+            opacity: 0.8;
+          }
+        `}
+      </style>
+    </form>
+  )
+}
---
+++ pages/api/checkout_sessions.js
@@ -0,0 +1,28 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create({
+        // customer_email: {},
+        // customer_email: 'test@123.com',
+        line_items: req.body.line_items,
+        billing_address_collection: 'auto',
+        shipping_address_collection: {
+          allowed_countries: ['GB'],
+        },
+        mode: 'payment',
+        success_url: `${req.headers.origin}/?success=true`, // TODO success page
+        cancel_url: `${req.headers.origin}/cart/?canceled=true`,
+      })
+      // res.redirect(303, session.url)
+      res.json({ url: session.url })
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message)
+    }
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
+  }
+}
