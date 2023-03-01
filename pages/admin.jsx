fa397711a33e16b5d25882f1b2b3dec7c1a53a19
Eloi-Perez <eloi104@hotmail.com>
2023-03-01 20:21:33 +0000

    feat: redirect if user is not admin

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,35 +1,76 @@
-import { useState, useContext, useEffect } from 'react'
+import { useState, useContext, useEffect, useMemo } from 'react'
+import { useRouter } from 'next/router'
 
 import { ProductsContext } from '../contexts/products-context'
 import AddProductForm from '../components/admin/add-product-form/add-product-form'
 import ProductCard from '../components/admin/product-card/product-card'
 import s from '../styles/admin.module.scss'
 
+function decodeJwt(token) {
+  const base64Payload = token.split(".")[1]
+  const payloadBuffer = Buffer.from(base64Payload, "base64")
+  return JSON.parse(payloadBuffer.toString())
+}
+
 export default function Admin() {
   const { products, fetchProducts } = useContext(ProductsContext)
   const [searchInput, setSearchInput] = useState('')
   const [openAdd, setOpenAdd] = useState(false)
+  const router = useRouter()
 
   useEffect(() => {
+    (async function () {
+      if (!await isAdmin()) {
+        router.push('/')
+      }
+    })();
     fetchProducts()
   }, [])
 
+  const isAdmin = async () => {
+    const credentials = localStorage.getItem('credentials')
+    if (!credentials) {
+      console.warn('Not authorized')
+      return false
+    }
+    const userID = decodeJwt(credentials)._id
+    try {
+      const call = await fetch(`/backend/v0/users/${userID}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${credentials}`
+        },
+      })
+      const response = await call.json()
+      if (response.admin) {
+        return true
+      } else {
+        console.warn('Not authorized')
+        return false
+      }
+    } catch (error) {
+      console.error('An unexpected error happened:', error)
+      return false
+    }
+  }
+
   return (
     <div className={s.main_container}>
-        <button onClick={() => setOpenAdd(!openAdd)}>Add new product</button>
-        {openAdd && <AddProductForm />}
-        <h1>Product list:</h1>
-        <input
-          type="search"
-          placeholder="Search here"
-          onChange={e => setSearchInput(e.target.value)}
-          value={searchInput} />
-        {products && !searchInput && products.map((product) => (
-          <ProductCard key={product._id} product={product} />
-        ))}
-        {products && searchInput && products.filter(li => li.name.toLowerCase().includes(searchInput.toLowerCase())).map((product) => (
-          <ProductCard key={product._id} product={product} />
-        ))}
+      <button onClick={() => setOpenAdd(!openAdd)}>Add new product</button>
+      {openAdd && <AddProductForm />}
+      <h1>Product list:</h1>
+      <input
+        type="search"
+        placeholder="Search here"
+        onChange={e => setSearchInput(e.target.value)}
+        value={searchInput} />
+      {products && !searchInput && products.map((product) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
+      {products && searchInput && products.filter(li => li.name.toLowerCase().includes(searchInput.toLowerCase())).map((product) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
     </div>
   )
 }
