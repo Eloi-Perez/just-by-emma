ad7272f0be9addd7844f70ff9d9fe9bf4c5fbd39
Eloi-Perez <eloi104@hotmail.com>
2023-03-16 19:45:46 +0000

    feat: className prop for div that wraps img

--- components/UI/emma-image/emma-image.jsx
+++ components/UI/emma-image/emma-image.jsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 import s from './emma-image.module.scss'
 import emma_img from '../../../public/img/emma.webp'
 
-export default function EmmaImage() {
+export default function EmmaImage({ className, ...props }) {
   return (
-    <div className={s.root}>
+    <div className={[s.root, className].join(' ')} {...props}>
       <Image src={emma_img} alt="Photo of Emma" width={300} height={300} />
     </div>
   )
