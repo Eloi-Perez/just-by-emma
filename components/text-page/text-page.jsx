2235d9d8d6096d7b2a0d1c79d4f207e531370c0b
Eloi-Perez <eloi104@hotmail.com>
2023-03-16 20:24:12 +0000

    style: text component

--- components/text-page/text-page.jsx
+++ components/text-page/text-page.jsx
@@ -6,12 +6,16 @@ export default function TextPage({ title, children }) {
   return (
     <>
       <HeaderImage />
-      <div>{title}</div>
-      <p>
-        If you would like to talk about your order or have any questions about Just by Emma products
-        or the brand, I am here to help you.
-      </p>
-      <EmmaImage />
+      <div className={s.header_content}>
+        <div className={s.header_content_left}>
+          <div className={s.title}>{title}</div>
+          <p className={s.header_text}>
+            If you would like to talk about your order or have any questions about Just by Emma products
+            or the brand, I am here to help you.
+          </p>
+        </div>
+        <EmmaImage />
+      </div>
       <div className={s.box}>{children}</div>
     </>
   )
