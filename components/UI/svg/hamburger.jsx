106d083fc5d5b4898762ee6b02f9f4d7f766b08f
Eloi-Perez <eloi104@hotmail.com>
2023-03-19 01:01:05 +0000

    feat: add svg & fix

---
+++ components/UI/svg/hamburger.jsx
@@ -0,0 +1,5 @@
+export default function Hamburger() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="16" viewBox="0 0 22 16" fill="none"><g><path fill="#4B4B4B" d="M.063 1.75c0-.518.42-.938.937-.938h20a.937.937 0 1 1 0 1.875H1a.937.937 0 0 1-.938-.937Zm0 6.25c0-.518.42-.938.937-.938h20a.937.937 0 1 1 0 1.875H1A.937.937 0 0 1 .062 8Zm0 6.25c0-.518.42-.938.937-.938h20a.937.937 0 1 1 0 1.876H1a.937.937 0 0 1-.938-.938Z" /></g><defs><path fill="#fff" d="M0 0h22v16H0z" /></defs></svg>
+  )
+}
